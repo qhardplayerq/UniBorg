3cdebdeaf11db2aaa95e2b1c0598193ddbbbd0f6
omwz5gsb@notua.com <omwz5gsb@notua.com>
2020-04-19 23:12:17 +0300

    .

---
+++ stdplugins/yavsak.py
@@ -0,0 +1,15 @@
+import logging
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
+from telethon import events
+from datetime import datetime
+from uniborg.util import admin_cmd
+
+@borg.on(admin_cmd(pattern="oc"))
+async def _(event):
+    await event.delete()
+    for a in range(1,11):
+        await borg.send_message(
+            event.chat_id,
+            "YAV YAV YAV YAV YAVVVVVŞŞAAAK"
+        )
