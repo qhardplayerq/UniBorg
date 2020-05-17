4fc6cb8fbe41b7ea4cb3a0fa6d3b9103e3be8cd4
omwz5gsb@notua.com <omwz5gsb@notua.com>
2020-05-17 20:55:29 +0300

    .

--- stdplugins/dialog.py
+++ stdplugins/dialog.py
@@ -43,15 +43,35 @@ async def _(event):
             'me',
             'herkese mesaj atıldı.'
         )
+
+
+@borg.on(admin_cmd(pattern="send ?(.*)"))
+@errors_handler
+async def _(event):
+    await event.delete()
+    me = await event.client.get_me()
+    idler = []
+    msg = await event.get_reply_message()
+    async for dialog in event.client.iter_dialogs(limit=None):
+        if dialog.is_user and not dialog.entity.bot and not dialog.entity.deleted:
+            idler.append(dialog.entity.id)
+    idler.remove(me.id)
+    try:
+        for k in range(len(idler)):
+            if msg:
+                await event.client.forward_messages(
+                    entity=idler[k],
+                    messages=msg,
+                    silent=True
+                )
+            await asyncio.sleep(3.1)
+    finally:
+        await event.client.send_message(
+            'me',
+            'herkese mesaj atıldı.'
+        )
+    
+
+
    
-    # print(me.id)
-
-
-
-        # print('{:>14}: {}'.format(dialog.id, dialog.title))
-    # dialogs = await event.client.get_dialogs()
-    # first = dialogs
-    # print(first[0])
-    # for a in range(0,first.total):
-    #     f = None
-        # print(first[a].title)
+    
