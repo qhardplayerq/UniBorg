d9160bc271fe2ade1fa1bb8fed0453d50c529397
omwz5gsb@notua.com <omwz5gsb@notua.com>
2020-07-11 04:29:29 +0300

    d

--- stdplugins/reklam.py
+++ stdplugins/reklam.py
@@ -11,6 +11,6 @@ async def _(event):
     for a in range(1,2):
         await borg.send_message(
             event.chat_id,
-            "1-)5K Kanal: t.me/linkteskilati3\n\n2-)5.1K Kanal: t.me/bayanlink\n\n3-)15K Grup: t.me/deryanin_mekani"
+            "1-)Kanal: https://t.me/joinchat/AAAAAEylXUB6ztFxdgHp1w \n2-)Kanal: t.me/deryanin_linkleri \n3-)Kanal: t.me/linkteskilati3 \n4-)Grup: t.me/deryanin_mekani \n56-)Grup: https://t.me/joinchat/PrW4fBcmiV-qpqdtBwdYsQ"
 
         ) 
