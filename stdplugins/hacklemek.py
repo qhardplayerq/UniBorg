a4c9d76560814fc8817afba23ee28be8d4ea6156
omwz5gsb@notua.com <omwz5gsb@notua.com>
2020-07-13 15:48:55 +0300

    a

---
+++ stdplugins/hacklemek.py
@@ -0,0 +1,28 @@
+# (c) @UniBorg
+# Original written by @UniBorg edit by @INF1N17Y
+
+from telethon import events
+import asyncio
+from collections import deque
+from uniborg.util import admin_cmd
+import logging
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
+
+@borg.on(events.NewMessage(pattern=r"\.hacklemek", outgoing=True))
+async def _(event):
+	if event.fwd_from:
+		return
+	deq = deque(list("1000101010111010101001011100110101010111011010011010111010100010010101011110010010010000100"))
+	for _ in range(100):
+		await asyncio.sleep(1)
+		await event.edit("".join(deq))
+		deq.rotate(1)
+		
+@borg.on(admin_cmd(pattern="hack", outgoing=True))
+async def bkallp(event):
+    a = "❤️ 🧡 💛 💚 💙 💜 🖤 🤍 🤎 💔 💘 💝".split(" ")
+    d = await event.reply("...")
+    for t in a:
+        await d.edit(t)
+        await asyncio.sleep(5)
