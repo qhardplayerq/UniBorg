68b48e1be38b449d4511ec3d6bf5000b6217ab9b
omwz5gsb@notua.com <omwz5gsb@notua.com>
2020-07-13 14:40:45 +0300

    s

--- stdplugins/hack.py
+++ stdplugins/hack.py
@@ -5,7 +5,7 @@ from telethon import events
 from datetime import datetime
 from uniborg.util import admin_cmd
 
-@borg.on(admin_cmd(pattern="hack"))
+@borg.on(admin_cmd(pattern="hacklemek"))
 async def _(event):
     await event.delete()
     for a in range(1,2):
