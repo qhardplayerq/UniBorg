c107d48bf5efbbab0e98fd193e023795724c5366
qhardplayerq <62712377+qhardplayerq@users.noreply.github.com>
2020-09-09 11:28:01 +0300

    Update trlink.py

--- stdplugins/trlink.py
+++ stdplugins/trlink.py
@@ -7,7 +7,7 @@ from uniborg.util import admin_cmd
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
-logger = logging.getLogger(__name__)
+logger = logging.getLor(__name__)
 
 
 
@@ -19,4 +19,28 @@ async def get_adzan(event):
         api = f"https://ay.link/api/?api=e2bb35a996ea8c9dfa4e5011005730bb584e283f&url={link}&alias&ct=1"
         r = requests.get(api)
         kisa_link = r.json()['shortenedUrl']
-        await event.edit("Kısaltılmış link:\n{}".format(kisa_link))
+        await event.edit("Kısaltılmış link:\n{}".format(import json
+import logging
+
+import requests
+
+from uniborg.util import admin_cmd
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
+logger = logging.getLogger(__name__)
+
+
+
+@borg.on(admin_cmd(pattern=("kisalt ?(.*)")))
+async def get_adzan(event):
+    
+    link = event.pattern_match.group(1)
+    if link:
+        api = f"https://ay.link/api/?api=e2bb35a996ea8c9dfa4e5011005730bb584e283f&url={link}&alias&ct=1"
+        api_2 = f"http://pubiza.com/api.php?token=snmmRSSLfiZQUGmfZox6k3UjQPEZpLa8&url={link}"
+        r = requests.get(api)
+        r_2 = requests.get(api_2)
+        kisa_link_2 = r.text
+        kisa_link = r.json()['shortenedUrl']
+        await event.edit("Kısaltılmış link:\n{}\nAlternatif link: {}".format(kisa_link,kisa_link_2))
