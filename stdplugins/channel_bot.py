e03e9f20bb1e9e707ead8ba647a1f5e26b74d093
qhardplayerq <62712377+qhardplayerq@users.noreply.github.com>
2020-09-09 12:48:17 +0300

    Update channel_bot.py

--- stdplugins/channel_bot.py
+++ stdplugins/channel_bot.py
@@ -52,7 +52,7 @@ async def get_media(event):
         )
         await asyncio.sleep(1.2)
         await event.client.send_message(
-            entity=await event.client.get_entity("@linkteskilati3"),
+            entity=await event.client.get_entity("https://t.me/joinchat/AAAAAE_oUYlNq2c45oQRlQ"),
             message=reply_message
         )
         await asyncio.sleep(1.2)
@@ -69,7 +69,7 @@ async def get_media(event):
         
         await asyncio.sleep(1.2)
         await event.client.send_message(
-            entity=await event.client.get_entity("https://t.me/joinchat/AAAAAE_oUYlNq2c45oQRlQ"),
+            entity=await event.client.get_entity("@linkteskilati3"),
             message=reply_message
         )
         
