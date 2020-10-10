84578ed9cea72ac653ce12b74523cf8574220f5c
qhardplayerq <62712377+qhardplayerq@users.noreply.github.com>
2020-10-11 00:23:21 +0300

    Update resend.py

--- stdplugins/resend.py
+++ stdplugins/resend.py
@@ -1,19 +1,11 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-import logging
-
-from telethon import events
-
-logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
-                    level=logging.WARNING)
-logger = logging.getLogger(__name__)
-
-
 @borg.on(events.NewMessage(pattern=r"\.resend", outgoing=True))
 async def _(event):
     await event.delete()
     m = await event.get_reply_message()
     if not m:
         return
+    await m.delete()
     await event.respond(m)
