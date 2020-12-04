3ac25a32dea5a601e994d884ca895b86ad812eb0
qhardplayerq <62712377+qhardplayerq@users.noreply.github.com>
2020-12-04 18:00:47 +0200

    Create smile.py

---
+++ stdplugins/smile.py
@@ -0,0 +1,34 @@
+﻿# (c) @UniBorg
+# Original written by @UniBorg edit by @INF1N17Y
+
+import asyncio
+import logging
+from collections import deque
+
+from telethon import events
+
+from uniborg.util import admin_cmd
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
+logger = logging.getLogger(__name__)
+
+
+@borg.on(events.NewMessage(pattern=r"\.smile", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    deq = deque(list("😃😄😁😆😅😂🤣🙃"))
+    for _ in range(100):
+        await asyncio.sleep(0.1)
+        await event.edit("".join(deq))
+        deq.rotate(1)
+
+
+@borg.on(admin_cmd(pattern="bsmile", outgoing=True))
+async def bkallp(event):
+    a = "😃 😄 😁 😆 😅 😂 🤣 🙃".split(" ")
+    d = await event.reply("...")
+    for t in a:
+        await d.edit(t)
+        await asyncio.sleep(5)
