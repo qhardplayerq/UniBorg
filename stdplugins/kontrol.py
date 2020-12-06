fb4df9e6e32561c0bb8b573d8078996d4aceb30d
qhardplayerq <62712377+qhardplayerq@users.noreply.github.com>
2020-12-06 21:06:29 +0200

    Update kontrol.py

--- stdplugins/kontrol.py
+++ stdplugins/kontrol.py
@@ -13,7 +13,7 @@ from uniborg.util import admin_cmd, progress
 async def test(event):
     j = await event.edit("`Bekle işlem yapılıyor 6 aylık mısın :)`")
     msg = ""
-    channel1 = -1001303403703
+    channel1 = -1001285905728
     stats1 = await event.client.get_stats(channel1)
     onceki1 = stats1.followers.previous
     # print(f"kanal 1 onceki kullanıcı sayısı {int(onceki1)} kişi")
@@ -25,7 +25,7 @@ async def test(event):
     # print(f"kanal 1 aradaki fark {int(fark1)} kişi")
     msg += f"kanal 1 aradaki fark {int(fark1)} kişi\n\n"
 
-    channel2 = -1001303403703
+    channel2 = -1001275030556
     stats2 = await event.client.get_stats(channel2)
     onceki2 = stats2.followers.previous
     # print(f"kanal 2 onceki kullanıcı sayısı {int(onceki2)} kişi")
@@ -37,7 +37,7 @@ async def test(event):
     # print(f"kanal 2 aradaki fark {int(fark2)} kişi")
     msg += f"kanal 2 aradaki fark {int(fark2)} kişi\n\n"
 
-    channel3 = -1001303403703
+    channel3 = -1001151369031
     stats3 = await event.client.get_stats(channel3)
     onceki3 = stats3.followers.previous
     # print(f"kanal 3 onceki kullanıcı sayısı {int(onceki3)} kişi")
@@ -49,7 +49,7 @@ async def test(event):
     # print(f"kanal 3 aradaki fark {int(fark3)} kişi")
     msg += "kanal 3 aradaki fark {int(fark3)} kişi\n\n"
 
-    channel4 = -1001303403703
+    channel4 = -1001469818787
     stats4 = await event.client.get_stats(channel4)
     onceki4 = stats4.followers.previous
     # print(f"kanal 4 onceki kullanıcı sayısı {int(onceki4)} kişi")
@@ -61,3 +61,4 @@ async def test(event):
     # print(f"kanal 4 aradaki fark {int(fark4)} kişi")
     msg += f"kanal 4 aradaki fark {int(fark4)} kişi\n"
     await j.edit(msg)
+    
