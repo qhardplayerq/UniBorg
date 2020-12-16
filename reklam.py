88edad2b0982019b01dee8e7ff0452e7ec726c7a
qhardplayerq <62712377+qhardplayerq@users.noreply.github.com>
2020-12-16 20:15:53 +0200

    Create reklam.py

---
+++ reklam.py
@@ -0,0 +1,15 @@
+import logging
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
+from telethon import events
+from datetime import datetime
+from uniborg.util import admin_cmd
+
+@borg.on(admin_cmd(pattern="linkler"))
+async def _(event):
+    await event.delete()
+    for a in range(1,2):
+        await borg.send_message(
+            event.chat_id,
+            "REKLAM SEÇENEKLERİMİZ:\n=========================\n20K KANAL👇\n6 saat 35 tl 3 saat ön plan sonra sabitleme\n12 saat 55tl 5 saat ön plan sonra sabitleme\n24 saat 90tl 10 saat ön plan sonra sabitleme\n\n50K KANAL👇\n6 saat 75 tl 3 saat ön plan sonra sabitleme\n12 saat 140tl 5 saat ön plan sonra sabitleme\n24 saat 250tl 10 saat ön plan sonra sabitleme\n\n220K KİTLE👇\n6 saat 120 tl 3 saat ön plan sonra sabitleme\n12 saat 200 tl 5 saat ön plan sonra sabitleme\n24 saat 350 10 saat ön plan sonra sabitleme\n========================="
+        )
