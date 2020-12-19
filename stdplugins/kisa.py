7cd9fb915e3136f630a6a93be5adedba92407eca
qhardplayerq <62712377+qhardplayerq@users.noreply.github.com>
2020-12-19 19:55:32 +0200

    Update kisa.py

--- stdplugins/kisa.py
+++ stdplugins/kisa.py
@@ -4,7 +4,7 @@ import requests
 # import aiohttp
 import asyncio
 from uniborg.util import admin_cmd
-import urllib.request 
+from urllib.request import Request, urlopen
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
 logger = logging.getLogger(__name__)
@@ -13,16 +13,19 @@ logger = logging.getLogger(__name__)
 
 @borg.on(admin_cmd(pattern=("kisalt ?(.*)")))
 async def get_adzan(event):
-  headers = {}
-  headers['User-Agent'] = 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17'
   link = event.pattern_match.group(1)
   if link:
       api = f"https://ay.link/api/?api=e2bb35a996ea8c9dfa4e5011005730bb584e283f&url={link}&alias&ct=1"
-      request = urllib.request.Request(api, headers=headers)
-      resp = urllib.request.urlopen(request)
-      data = json.loads(resp.read())
-      msg = data['shortenedUrl']
-      await event.edit(msg)
+      req = Request(api, headers={'User-Agent': 'Mozilla/5.0'})
+      web_byte = urlopen(req).read()
+      webpage = web_byte.decode('utf-8')
+      data = json.loads(webpage)
+      await event.edit(data['shortenedUrl'])
+
+
+
+
+
 
 
         
