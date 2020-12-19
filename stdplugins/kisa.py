754233e1add5e1c3e52c0fa09a6ca1d2e26e0b39
qhardplayerq <62712377+qhardplayerq@users.noreply.github.com>
2020-12-19 19:46:59 +0200

    Update kisa.py

--- stdplugins/kisa.py
+++ stdplugins/kisa.py
@@ -15,15 +15,14 @@ logger = logging.getLogger(__name__)
 async def get_adzan(event):
   headers = {}
   headers['User-Agent'] = 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17'
-    
-    link = event.pattern_match.group(1)
-    if link:
-        api = f"https://ay.link/api/?api=e2bb35a996ea8c9dfa4e5011005730bb584e283f&url={link}&alias&ct=1"
-        request = urllib.request.Request(api, headers=headers)
-        resp = urllib.request.urlopen(request)
-        data = json.loads(resp.read())
-        msg = data['shortenedUrl']
-        await event.edit(msg)
+  link = event.pattern_match.group(1)
+  if link:
+      api = f"https://ay.link/api/?api=e2bb35a996ea8c9dfa4e5011005730bb584e283f&url={link}&alias&ct=1"
+      request = urllib.request.Request(api, headers=headers)
+      resp = urllib.request.urlopen(request)
+      data = json.loads(resp.read())
+      msg = data['shortenedUrl']
+      await event.edit(msg)
 
 
         
