eb034b7e153570b0bca83f634d73a466e8de7de1
qhardplayerq <62712377+qhardplayerq@users.noreply.github.com>
2020-12-19 19:25:40 +0200

    Update kisa.py

--- stdplugins/kisa.py
+++ stdplugins/kisa.py
@@ -4,7 +4,7 @@ import requests
 # import aiohttp
 import asyncio
 from uniborg.util import admin_cmd
-
+import urllib.request 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
 logger = logging.getLogger(__name__)
@@ -17,10 +17,10 @@ async def get_adzan(event):
     link = event.pattern_match.group(1)
     if link:
         api = f"https://ay.link/api/?api=e2bb35a996ea8c9dfa4e5011005730bb584e283f&url={link}&alias&ct=1"
-        response = requests.get(api).json()
-        if response['status'] == 'success':
-          msg = response['shortenedUrl']
-          await event.edit(f"Kısa link: {msg}")
+        with urllib.request.urlopen(api) as url:
+            data = json.loads(url.read().decode())
+            msg = data['shortenedUrl']
+            await event.edit(msg)
 
 
         
